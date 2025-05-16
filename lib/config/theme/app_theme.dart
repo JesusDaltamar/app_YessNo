01d928122a6ec68ac769e8363f4b5b0b72ba4f4a
Jesus Rodriguez <jesusrodaltamar@gmail.com>
2025-05-15 19:29:26 -0500

    Creacion de aplicacion y de clase que permite cambiar entre temas visuales

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0x0F572DCC);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.black,
+  Colors.orange,
+  Colors.pink,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+    : assert(
+        selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,
+        "colors must be between 0 and ${_colorThemes.length}",
+      );
+
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[selectedColor],
+      brightness: Brightness.dark,
+    );
+  }
+}
